aa00933dcd157648d30711e1292af36db2cc76ab
kevin-MAC <kevin@kevin-MACdeMacBook-Pro.local>
2015-05-29 13:42:48 +0800

    Conclusion

---
+++ include/rrl_priv.h
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ *
+ * Name:          rrl_priv.h
+ * Project:       RRL Core
+ * Purpose:       Definition of cla;sses of registration
+ * Author:        Wang Jicheng, wangjicheng11@163.com
+ * Version:       2015-4-27 0.01
+ *
+ *******************************************************************************
+ * The License and Describe
+ *
+ *******************************************************************************/
+#ifndef RRLDATASET_HH
+#define RRLDATASET_HH
+
+
+#include <gdal_priv.h>
+
+/**
+ * @
+ */
+class RRLDataset
+{
+private:
+};
+
+#endif
